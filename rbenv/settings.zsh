026b0986bd1a40e3c5291dd3bc61b7447944f884
Jordan Glasner <glasner@gmail.com>
2012-11-05 10:33:47 -0500

    removing rvm/fixing path

---
+++ rbenv/settings.zsh
@@ -0,0 +1,5 @@
+# enable shims and autocompletion 
+if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
+
+# use Homebrew's directories rather than ~/.rbenv 	
+export RBENV_ROOT=/usr/local/opt/rbenv
--- system/path.zsh
+++ system/path.zsh
@@ -1,1 +1,1 @@
-PATH=/usr/local/bin:$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
+PATH=/usr/local/bin:$PATH:~/.dotfiles/bin 
