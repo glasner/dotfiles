ed0e9e869bb2413b4e43aae8aac18ecf7e329da2
Jordan Glasner <glasner@gmail.com>
2012-12-25 11:47:35 -0500

    created hub specific settings

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,11 +1,3 @@
-# Use `hub` as our git wrapper:
-#   http://defunkt.github.com/hub/
-hub_path=$(which hub)
-if [[ -f $hub_path ]]
-then
-  alias git=$hub_path
-fi
-
 alias pull='git pull'
 alias push='git push'
 alias gc='git add -A; git commit -m'
