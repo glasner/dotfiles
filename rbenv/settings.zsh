ea9b92c1f688d523dbdd89ce5e9a8e47ff83091f
Jordan Glasner <glasner@gmail.com>
2016-04-01 09:45:00 -0400

    Setup dev machine

--- rbenv/settings.zsh
+++
@@ -1,6 +0,0 @@
-# use Homebrew's directories rather than ~/.rbenv 	
-export RBENV_ROOT=/usr/local/opt/rbenv
-
-# enable shims and autocompletion 
-if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
-
